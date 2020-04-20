69f0dadbdca8efa1f32a60140abea8a9a1ea721b
chiajee <leechia.jee97@gmail.com>
2020-04-20 14:53:44 +0800

    test

--- index.js
+++ index.js
@@ -1106,11 +1106,9 @@ app.post('/getForumPost/',(req,res,next)=>{
 	var jsonArray=[];
 	var parentID = '';
 	con.query('SELECT forum.* ,'+
-		'user.photo , '+
-		// 'specialist.photo '+
+		'user.photo '+
 		'FROM forumdata forum '+
 		'LEFT JOIN usertable user USING(email) '+
-		// 'LEFT JOIN specialisttable specialist USING(email) '+	
 		'WHERE forum.parentID=? ORDER BY id DESC LIMIT 30',
 		[parentID],
 		function(err,result,fields){
