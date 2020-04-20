960aa40923fdb871f8dedcac5a459d680a6d9d7b
chiajee <leechia.jee97@gmail.com>
2020-04-20 13:38:46 +0800

    fix 4

--- index.js
+++ index.js
@@ -1106,7 +1106,7 @@ app.post('/getForumPost/',(req,res,next)=>{
 	var jsonArray=[];
 	var parentID = '';
 	con.query('SELECT forum.* ,'+
-		'user.photo, '+
+		'user.photo '+
 		'FROM forumdata forum '+
 		'LEFT JOIN usertable user USING(email) '+
 		'WHERE forum.parentID=? ORDER BY id DESC LIMIT 30',
